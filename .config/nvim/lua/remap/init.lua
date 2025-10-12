2a4f2e018bebbf6f5faef7a6ece0ffe8241b9599
nikishefu <nikitaardashev2004@yandex.ru>
2025-10-12 11:46:36 +0300

    diag float remap

--- .config/nvim/lua/remap/init.lua
+++ .config/nvim/lua/remap/init.lua
@@ -3,4 +3,4 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.api.nvim_set_keymap("n", "<CR>", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts)
-vim.api.nvim_set_keymap("n", "<leader>D", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>dv", "<Cmd>lua vim.diagnostic.open_float()<CR>", opts) -- Diag view
